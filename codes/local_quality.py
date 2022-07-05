303ce2c27d76bf6d7c33fb23f05fd07aa76dc2c5
Negin Piran Nanekaran <npiran@cedar1.cedar.computecanada.ca>
2022-07-04 18:24:36 -0700

    conflict resolved

--- codes/local_quality.py
+++ codes/local_quality.py
@@ -66,8 +66,9 @@ def main(model_name, run_note):
     history = model.fit_generator(
             train_generator,
             steps_per_epoch = len(X_train)//batch_size,
-            epochs = 2,
             # class_weight = class_weight,
+            epochs = 200,
+           # class_weight = {0:10.37, 1:1.35, 2:.46},
             validation_data = (X_test, y_test)
     )
 
@@ -91,5 +92,5 @@ def main(model_name, run_note):
 if __name__=="__main__":
     model_names = {'ResNet50', 'InceptionV3', 'VGG16'}
     for model_name in model_names:
-        run_note = model_name + "NoWeights_100"
+        run_note = model_name + "Weights_200"
         main(model_name, run_note)
