e9321722a6e18a7f53606d377247b830f88c1864
neginprn <piran.negin.n@gmail.com>
2022-08-27 12:53:51 -0400

    for loop

--- codes/train_iqa.py
+++ codes/train_iqa.py
@@ -19,10 +19,23 @@ is_binary = True
 def load_data():
     x_train = np.load('../data/X_quality_train.npy')
     y_train = np.load('../data/y_quality_train.npy')
-    y_train[y_train==0] = 1 # replace class 0 with 1
+    y_train = y_train.astype(int)
     x_test = np.load('../data/X_quality_test.npy')
     y_test = np.load('../data/y_quality_test.npy')
-    y_test[y_test==0] = 1  # replace class 0 with 1
+    y_test = y_test.astype(int)
+    # Change 3 classes (0, 1, and 2) to binary classification 
+    y_train[y_train==1] = 0 
+    y_train[y_train==2] = 1 
+    y_test[y_test==1] = 0  
+    y_test[y_test==2] = 1 
+    
+    print(f'unique: {np.unique(y_train)}')
+    print(f'unique: {np.unique(y_train)}')
+    
+    # y_train[y_train==0] = 1 
+    # y_test[y_test==0] = 1  
+
+    
     # x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle = True)
     # print(f'x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}, x_train.max: {x_train.max()}, x_test.max: {x_test.max()}')
     return x_train, x_test, y_train, y_test
@@ -42,7 +55,7 @@ def train_iqa(image_features, y, method='random_forest'):
     elif method=='svm':
         model = svm.SVC(decision_function_shape='ovo')  #For multiclass classification
 
-    print(f'before fit: X_for_RF.shape: {X_for_RF.shape}, y_train.shape: {y_train.shape}')
+    # print(f'before fit: X_for_RF.shape: {X_for_RF.shape}, y_train.shape: {y_train.shape}')
     # Fit the model on training data
     model.fit(X_for_RF, y_train) #For sklearn no one hot encoding
     joblib.dump(model, "../models/patient_model.joblib")
@@ -85,6 +98,7 @@ if __name__ == "__main__":
     x_train, x_test, y_train, y_test = load_data()
     #Extract features from training images
     image_features = feature_extractor(x_train)
-    print(f'image_features.shape: {image_features.shape}')
-    model = train_iqa(image_features, y_train, 'svm')
-    test_model(x_test, y_test, model, run_note)
+    for i in range(10):
+        print(f'------> i: {i} image_features.sum: {image_features.sum()}')
+        model = train_iqa(image_features, y_train, 'svm')
+        test_model(x_test, y_test, model, f'i_{run_note}')
